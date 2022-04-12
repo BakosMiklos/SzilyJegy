0c7b022b752e23e79726891b4cdb98a3d0c94578
Gamika18 <61663882+Gamika18@users.noreply.github.com>
2022-04-12 10:07:14 +0200

    Add files via upload

--- rendezveny_feltoltes.php
+++ rendezveny_feltoltes.php
@@ -1,20 +1,18 @@
 <!DOCTYPE html>
 <html lang="hu">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Rendezvény feltöltés</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rendezvény feltöltés</title>
 </head>
 <body>
 <?php
-
 session_start();
 error_reporting(0);
 include("header/headerRegisztracio/kapcs.inc.php");
 ?>
-
-  <form method='post' enctype='multipart/form-data'>
+    <form method='post' enctype='multipart/form-data'>
     <a href="index.php"><h1>Főoldal <<<</h1></a>
     Rendezvény neve: <input type='text' name='nev'><br>
     Rendezvény dátuma: <input type='date' name='datum'><br>
@@ -29,67 +27,66 @@ include("header/headerRegisztracio/kapcs.inc.php");
     Eladható jegyek száma: <input type='text' name='eladhatojegyekszama'><br>
     Ár: <input type='text' name='jegyar'><br>
     <br><input type='submit' name='gomb' value='Rögzítés'><br>
-  </form>
-
-<?php
+    </form>
+    <?php
 if(isset($_POST['gomb']))
 {
-  $nev = $_POST['nev'];
-  $reszvetelimod = $_POST['reszvetelimod'];
-  $leiras = $_POST['leiras'];
-  $rendezvenydatuma = $_POST['datum'];
-  $rendezvenyhelyszin = $_POST['helyszin'];
-  $eladhatojegyekszama = $_POST['eladhatojegyekszama'];
-  $jegyar = $_POST['jegyar'];
+    $nev = $_POST['nev'];
+    $reszvetelimod = $_POST['reszvetelimod'];
+    $leiras = $_POST['leiras'];
+    $rendezvenydatuma = $_POST['datum'];
+    $rendezvenyhelyszin = $_POST['helyszin'];
+    $eladhatojegyekszama = $_POST['eladhatojegyekszama'];
+    $jegyar = $_POST['jegyar'];
 
-  $target_dir = "kepek/";
-  $target_file = $target_dir . basename($_FILES["kep"]["name"]);
-  $uploadOk = 1;
-  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    $target_dir = "kepek/";
+    $target_file = $target_dir . basename($_FILES["kep"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
     
-  // Check if image file is a actual image or fake image
-  if(isset($_POST["gomb"])) {
-    $check = getimagesize($_FILES["kep"]["tmp_name"]);
-    if($check !== false) {
-      $uploadOk = 1;
-    } else {
-      echo "A fájl nem kép.";
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["gomb"])) {
+      $check = getimagesize($_FILES["kep"]["tmp_name"]);
+      if($check !== false) {
+        $uploadOk = 1;
+      } else {
+        echo "A fájl nem kép.";
+        $uploadOk = 0;
+      }
+    }
+    
+    // Check if file already exists
+    if (file_exists($target_file)) {
+      echo "A fájl már létezik.";
       $uploadOk = 0;
     }
-  }
     
-  // Check if file already exists
-  if (file_exists($target_file)) {
-    echo "A fájl már létezik.";
-    $uploadOk = 0;
-  
+    // Check file size
+    if ($_FILES["kep"]["size"] > 5000000) {
+      echo "Túl nagy fájl.";
+      $uploadOk = 0;
+    }
     
-  // Check file size
-  if ($_FILES["kep"]["size"] > 5000000) {
-    echo "Túl nagy fájl.";
-    $uploadOk = 0;
-  }
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+      echo "Csak JPG, JPEG, PNG fájlokat lehet feltölteni.";
+      $uploadOk = 0;
+    }
     
-  // Allow certain file formats
-  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-    echo "Csak JPG, JPEG, PNG fájlokat lehet feltölteni.";
-    $uploadOk = 0;
-  }
-    
-  // Check if $uploadOk is set to 0 by an error
-  if ($uploadOk == 0) {
-    echo "Nem sikerült feltölteni a fájlt.";
-  // if everything is ok, try to upload file
-  } else {
-    if (move_uploaded_file($_FILES["kep"]["tmp_name"], $target_file)) {
-      echo htmlspecialchars( basename( $_FILES["kep"]["name"])). " fájl feltöltve.";
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+      echo "Nem sikerült feltölteni a fájlt.";
+    // if everything is ok, try to upload file
+    } else {
+      if (move_uploaded_file($_FILES["kep"]["tmp_name"], $target_file)) {
+        echo htmlspecialchars( basename( $_FILES["kep"]["name"])). " fájl feltöltve.";
 
-      $fileExt = explode('.', $kep);
-      $fileActualExt = strtolower(end($fileExt));
-      $fileNameNew = uniqid('', true).".".$fileActualExt;
-      $fileDestination = 'kepek/'.$fileNameNew;
+        $fileExt = explode('.', $kep);
+        $fileActualExt = strtolower(end($fileExt));
 
-      move_uploaded_file($check, $fileDestination);
+        $fileNameNew = uniqid('', true).".".$fileActualExt;
+        $fileDestination = 'kepek/'.$fileNameNew;
+        move_uploaded_file($check, $fileDestination);
       } else {
         echo "Valami baj történt a fájl feltöltése során.";
       }
@@ -101,8 +98,7 @@ if(isset($_POST['gomb']))
       VALUES  (1,'$nev','$reszvetelimod','$rendezvenydatuma','$rendezvenyhelyszin','$leiras', '$jegyar','$kep', '$eladhatojegyekszama', 0)";
       mysqli_query($con,$query) or die ('Hiba az adatbevitelnél!');
       print("<br>A rendezvény adatai feltöltve!");
-    }
+}
 ?>
-
 </body>
 </html>
